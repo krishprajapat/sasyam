49288c8b54397aba25c4ea2f5eb8043fb8905beb
krish <prajapatikrish367@gmail.com>
2025-06-06 12:46:36 +0530

    update

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,6 +2,6 @@ export const config = {
   apiBaseUrl:
     import.meta.env.VITE_BACKEND_URL ||
     (process.env.NODE_ENV === "production"
-      ? "https://sasyamrita-backend.onrender.com"
-      : "http://localhost:5000"),
+      ? "https://sasyam-p58h.vercel.app" // Vercel backend URL
+      : "http://localhost:5000"), // Local development URL
 };
